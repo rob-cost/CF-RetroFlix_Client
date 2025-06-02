3e08dd01c7a472b356019f4d69a5a33a09ca29de
rob-cost <robertocostantino3@gmail.com>
2025-06-02 10:44:42 +0200

    Updated components

--- src/components/Login-View/login-view.jsx
+++ src/components/Login-View/login-view.jsx
@@ -1,17 +1,62 @@
 import React from 'react'
+import { useState } from 'react';
 
-export const LoginView = () => {
+
+
+export const LoginView = (onLoggedIn) => {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const data = {
+            access: username,
+            secret: password
+        }
+        fetch('https://my-vintage-flix-06cde8de3bcb.herokuapp.com/login', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log("Login response: ", data);
+                if(data.user) {
+                    onLoggedIn(data.user, data.token);
+                } else {
+                    alert ('User does not exist')
+                }
+            })
+            .catch ((e) => {
+                alert('Something is wrong' + e);
+            });
+    };
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>
                 Username:
-                <input type='text' />
+                <input
+                    type='text'
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                />
             </label>
             <label>
                 Password:
-                <input type='password' />
+                <input
+                    type='password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
             </label>
-            <button type='submit'>Submit</button>
+            <button type='submit'>Login</button>
         </form>
     )
 }
--- src/components/Main-View/main-view.jsx
+++ src/components/Main-View/main-view.jsx
@@ -2,17 +2,27 @@ import { useEffect, useState } from "react";
 import { MovieCard } from "../Movie-Card/movie-card";
 import { MovieView } from "../Movie-View/movie-view";
 import { LoginView } from "../Login-View/login-view";
+import { SignupView } from "../Signup-View/signup-view";
 
 export const MainView = () => {
   const [movies, setMovies] = useState([]);
   const [selectMovie, setSelectMovie] = useState(null);
   const [similarMovies, setSimilarMovies] = useState([]);
   const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
 
   // Fetching list of movies from the API
   useEffect(() => {
-    fetch('https://my-vintage-flix-06cde8de3bcb.herokuapp.com/movies')
+    if (!token) {
+      return;
+    }
+    fetch('https://my-vintage-flix-06cde8de3bcb.herokuapp.com/movies', {
+      headers: { Authorization: `Bearer ${token}` }
+    })
       .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
       .then((movie) => {
         const movieFromApi = movie.map((movie) => {
           return {
@@ -30,7 +40,7 @@ export const MainView = () => {
         setMovies(movieFromApi);
       });
 
-  }, [])
+  }, [token])
 
 
   // Creating a list of movies with the same genre of a selected one 
@@ -45,7 +55,16 @@ export const MainView = () => {
 
   // Login form
   if (!user) {
-    return <LoginView />;
+    return (
+      <>
+        <LoginView onLoggedIn={(user, token) => {
+          setUser(user);
+          setToken(token);
+        }} />
+        or
+        <SignupView />
+      </>
+    );
   }
 
   // Renders selected Movie
@@ -63,12 +82,12 @@ export const MainView = () => {
         <ul>
           {similarMovies.map((movie) => (
             <li key={movie.id}
-            onClick={()=> {
-              setSelectMovie(movie)
-            }}
+              onClick={() => {
+                setSelectMovie(movie)
+              }}
             >
-            {movie.Title}
-          </li>))}
+              {movie.Title}
+            </li>))}
         </ul>
       </>
     );
