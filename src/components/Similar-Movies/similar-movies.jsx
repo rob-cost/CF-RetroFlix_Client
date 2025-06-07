2eccef042515fe0736751a0d5eb9d80d0a301512
rob-cost <robertocostantino3@gmail.com>
2025-06-07 12:44:03 +0200

    Updated similar-movies
    Implemented movie-card component into similar movies

--- src/components/Similar-Movies/similar-movies.jsx
+++ src/components/Similar-Movies/similar-movies.jsx
@@ -1,38 +1,30 @@
 import { Card, CardImg, Container, Row, Col } from "react-bootstrap"
 import { useParams, Link } from "react-router-dom";
+import { MovieCard } from "../Movie-Card/movie-card";
 
 
 
-export const SimilarMovies = ({ movies }) => {
-  console.log(movies)
-  const {title} = useParams();
+export const SimilarMovies = ({ movies, token }) => {
+  const { title } = useParams();
   const decodedTitle = decodeURIComponent(title);
-  const movie = movies.find((m)=>m.Title===decodedTitle);
-  const genre = movie.Genre.Name;
-  const simMovies = movies.filter(m => m.Genre.Name === genre && m.Title !== title)
+  const movie = movies.find((m) => m.Title === decodedTitle);
+  const genre = movie.Genre?.Name;
+  const simMovies = movies.filter(m => m.Genre?.Name === genre && m.Title !== title)
 
   return (
     <Container>
       <Row>
         <h2>Similar Movies</h2>
-        {simMovies.map((movie) => (
-          <Col key={movie.id} md={3} className="mb-4, mt-2">
-           <Link to ={`/movies/${encodeURIComponent(movie.Title)}`}>
-            <Card 
-            className="h-100" 
-            >
-              <CardImg 
-              variant="top" 
-              className="img-fluid"
-              src={movie.Image}  />
-              <Card.Body>
-                <Card.Title>{movie.Title}</Card.Title>
-                <Card.Text>{movie.Description}</Card.Text>
-              </Card.Body>
-            </Card>
-            </Link>
-          </Col>
-        ))}
+        {simMovies.map((m) => {
+          return (
+            <Col className="mb-3" md={3} key={m.id}>
+              <MovieCard
+                movie={m}
+                token={token}
+              />
+            </Col>
+          );
+        })}
       </Row>
     </Container>
   )
