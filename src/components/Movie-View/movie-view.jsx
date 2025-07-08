301e64be39e1468a1a3e74eb7921da87328e1fd8
rob-cost <robertocostantino3@gmail.com>
2025-07-08 14:46:56 +0200

    Updated movie-view

--- src/components/Movie-View/movie-view.jsx
+++ src/components/Movie-View/movie-view.jsx
@@ -1,36 +1,42 @@
-import { Container, Row, Col, Button, Modal, OverlayTrigger, Tooltip } from "react-bootstrap";
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Modal,
+  OverlayTrigger,
+  Tooltip,
+} from "react-bootstrap";
 import { Link, useParams } from "react-router-dom";
 import { UseMovieAction } from "../Use-Movie-Action/useMovieAction";
 
-
 export const MovieView = ({ movies, token, favoriteChange }) => {
-
   const { title } = useParams();
   const decodedTitle = decodeURIComponent(title);
   const movie = movies.find((m) => m.Title === decodedTitle);
-  
+
   const {
     isFavorite,
     toWatch,
     addToFavorites,
     removeFromFavorites,
     addToWatch,
-    removeFromToWatch
-  } = UseMovieAction ({movie, token, favoriteChange});
-
+    removeFromToWatch,
+  } = UseMovieAction({ movie, token, favoriteChange });
 
   if (!movie) {
     return (
       <Container>
         <div>Movie not found</div>
       </Container>
-    )
-  };
+    );
+  }
 
-  const genreTooltip =
-    <Tooltip id="genre-tooltip" >
-      {movie.Genre?.Description || 'Unknown'}
+  const genreTooltip = (
+    <Tooltip id="genre-tooltip">
+      {movie.Genre?.Description || "Unknown"}
     </Tooltip>
+  );
 
   return (
     <Container>
@@ -39,32 +45,18 @@ export const MovieView = ({ movies, token, favoriteChange }) => {
           <div>
             <h1>{movie.Title}</h1>
             <p className="d-flex justify-content-between">
-
               <OverlayTrigger
                 placement="right"
-                overlay={
-                  <Tooltip>{movie.Director?.Bio}</Tooltip>
-                }
+                overlay={<Tooltip>{movie.Director?.Bio}</Tooltip>}
               >
-                <Link>
-                  {movie.Director?.Name}
-                </Link>
+                <Link>{movie.Director?.Name}</Link>
               </OverlayTrigger>
 
-
-              <OverlayTrigger
-                placement="left"
-                overlay={genreTooltip}
-              >
-                <Link
-                  variant="success"
-                >
-                  {movie.Genre?.Name}
-                </Link>
+              <OverlayTrigger placement="left" overlay={genreTooltip}>
+                <Link variant="success">{movie.Genre?.Name}</Link>
               </OverlayTrigger>
             </p>
             <p>{movie.Release}</p>
-
 
             <p>"{movie.Description}"</p>
 
@@ -76,57 +68,53 @@ export const MovieView = ({ movies, token, favoriteChange }) => {
                     placement="bottom"
                     overlay={
                       <Tooltip id={`actor-tooltip-${index}`}>
-                        {actor.Bio || 'Unknown'}
+                        {actor.Bio || "Unknown"}
                       </Tooltip>
                     }
                   >
-                    <Link>{actor.Name}  </Link>
+                    <Link>{actor.Name} </Link>
                   </OverlayTrigger>
-                )
+                );
               })}
             </p>
 
             {/*   <p>{movie.Rating}</p> */}
           </div>
           <div className="d-flex justify-content-between align-items-center">
-          <Link to={'/movies'}>
-            <Button
-              className="btn align-self-start"
-
-            >Back</Button>
-          </Link>
-          <div className="d-flex gap-2">
-          {!toWatch ? (
-            <Button onClick={addToWatch}
-            className="btn"
-              >
-              + To Watch
-            </Button>
-          ) : (
-            <Button
-              onClick={removeFromToWatch}
-              className="btn"
-              >
-              - Remove
-            </Button>
-          )}
-          {!isFavorite ? (
-            <Button 
-            onClick={addToFavorites}
-            className="bi bi-heart btn">
-            </Button>
-          ) : (
-            <Button 
-            onClick={removeFromFavorites}
-            className="bi bi-heart-fill btn">
-            </Button>
-          )}
-          </div>
+            <Link to={"/movies"}>
+              <Button className="btn align-self-start">Back</Button>
+            </Link>
+            <div className="d-flex gap-2">
+              {!toWatch ? (
+                <Button onClick={addToWatch} className="btn">
+                  + To Watch
+                </Button>
+              ) : (
+                <Button onClick={removeFromToWatch} className="btn">
+                  - Remove
+                </Button>
+              )}
+              {!isFavorite ? (
+                <Button
+                  onClick={addToFavorites}
+                  className="bi bi-heart btn"
+                ></Button>
+              ) : (
+                <Button
+                  onClick={removeFromFavorites}
+                  className="bi bi-heart-fill btn"
+                ></Button>
+              )}
+            </div>
           </div>
         </Col>
         <Col md={5}>
           <div className="position-relative">
-            <img src={movie.Image} alt={movie.Title} style={{ maxHeight: '400px', width: '100%' }} />
+            <img
+              src={movie.Image}
+              alt={movie.Title}
+              style={{ maxHeight: "400px", width: "100%" }}
+            />
           </div>
         </Col>
       </Row>
