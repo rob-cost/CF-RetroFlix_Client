1e4c3b90c8b8e12abaa2ba526764382250053368
rob-cost <robertocostantino3@gmail.com>
2025-09-15 17:17:24 +0200

    Fixed color navbar toggle

--- src/components/Navigation-Bar/navigation-bar.jsx
+++ src/components/Navigation-Bar/navigation-bar.jsx
@@ -1,27 +1,26 @@
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 export const NavScroll = ({ setUser, setToken, searchItem, setSearchItem }) => {
-
-const handleSearch = (e) => {
-  e.preventDefault()
-  const inputType = e.target.value
-  setSearchItem(inputType)
-}
-
+  const handleSearch = e => {
+    e.preventDefault();
+    const inputType = e.target.value;
+    setSearchItem(inputType);
+  };
 
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container fluid>
-        <Navbar.Brand href="/movies"><h1>RetroFlix</h1></Navbar.Brand>
+        <Navbar.Brand href="/movies">
+          <h1>RetroFlix</h1>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
+            style={{ maxHeight: "100px" }}
             navbarScroll
           >
             <Nav.Link href="/movies">Home</Nav.Link>
@@ -36,16 +35,18 @@ const handleSearch = (e) => {
               className="me-2"
               aria-label="Search"
             />
-            </Form>
-            <Nav.Link onClick={() => {
+          </Form>
+          <Nav.Link
+            onClick={() => {
               setUser(null);
               setToken(null);
               localStorage.clear();
-            }}>Logout</Nav.Link>
-          
+            }}
+          >
+            Logout
+          </Nav.Link>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
-}
-
+};
